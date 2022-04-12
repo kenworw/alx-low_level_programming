e2d6696d7bf0b3b29cd1d1370d32a3a037ac2106
kenworw <kenworw@users.noreply.github.com>
2022-04-12 20:55:55 +0300

    Fix out put task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,37 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * times_table - prints the 9 times table, starting with 0
- *
- *
- * Return: no return value
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void times_table(void)
 {
-	int number = 0;
-	int multiplier = 0;
+	int r, c, d;
 
-	for (multiplier = 0; multiplier < 10; multiplier++)
+	for (r = 0; r <= 9; r++)
 	{
-		for (number = 0; number < 10; number++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
 		{
-			if (number > 0)
-			{
-				printf(", ");
-			}
-			printf("%2d", number * multiplier);
+			d = (r * c);
+		if ((d / 10) > 0)
+		{
+			_putchar((d / 10) + '0');
 		}
-		printf("\n");
+		else
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
+		if (c < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+		_putchar('\n');
 	}
 }
