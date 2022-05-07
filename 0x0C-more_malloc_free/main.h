7efa7fe9edaf433484c8bf090aeb8b544a148a80
kenworw <kenworw@users.noreply.github.com>
2022-05-07 20:17:48 +0300

    Fix_tupdate main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
