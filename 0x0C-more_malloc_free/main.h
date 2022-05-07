f51880d8d33f25433e90d61ef0850bdf8c09fdd2
kenworw <kenworw@users.noreply.github.com>
2022-05-07 21:29:25 +0300

    update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,27 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAINN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-int len(char *str);
-int check_for_digits(char **av)
-void init(char *str, int l)	
-int main(int argc, char *argv[])
-void _print(char *str, int l)	
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 
-#endif /*MAIN_H*/
+#endif
