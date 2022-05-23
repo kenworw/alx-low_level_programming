6205863418636834a962324b3634d7d4925b9688
kenworw <kenworw@users.noreply.github.com>
2022-05-23 22:56:28 +0300

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
+
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of elements
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
+ *
+ * Return: number of elements in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	register size_t count = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list,
- * and returns the head node’s data (n)
- * @head: double pointer to head of list;
- * Return: value at head
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: double pointer to the beginning of the list
+ *
+ * Return: the head node's data (n).
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *current;
-	int res;
+	listint_t *first;
+	int n;
 
-	if (!(*head) || !head)
+	if (head == NULL || *head == NULL)
 		return (0);
-	current = *head;
-	res = current->n;
-	*head = (*head)->next;
-	free(current);
-	return (res);
+	first = *head;
+	*head = first->next;
+	n = first->n;
+	free(first);
+	return (n);
 }
